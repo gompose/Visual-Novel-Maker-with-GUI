e06c1b4977fcaef0d7f2352a3a19320ce7af7384
gompose <peter.m12321@gmail.com>
2017-01-09 12:13:59 -0500

    Update Driver.java
    Test Cases

---
+++ Driver.java
@@ -0,0 +1,14 @@
+public class Driver {
+  public static void main (String[] args) {
+  
+  //"Book" starts here
+  Slide x1 = new Slide("Hello World!","","");
+  Slide x2 = new Slide("Testing 123!","","");
+  
+  //Testing
+  System.out.println(x1.getText);
+  System.out.println(x2.getText);
+  
+  
+  }  
+}
