58629426aa40b0f878a4a0e03405860cadcca45b
gompose <peter.m12321@gmail.com>
2017-01-09 12:35:55 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,14 +1,23 @@
 public class Driver {
-  public static void main (String[] args) {
+  //Instance Variables
+  int page;
+  
+  public Driver() {
+    page = 0;
+  }
   
   //"Book" starts here
-  Slide x1 = new Slide("Hello World!","","");
-  Slide x2 = new Slide("Testing 123!","","");
+  Slide page1 = new Slide("Hello World!","","");
+  Slide page2 = new Slide("Testing 123!","","");
   
   //Testing
   System.out.println(x1.getText);
   System.out.println(x2.getText);
+  //Methods
+  public void createPage(String text, String picture, String sound) {
+    String _page;
+  }
   
   
-  }  
+   
 }
