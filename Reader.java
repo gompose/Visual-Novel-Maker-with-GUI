9a561408a00a84388751b886806d884d88e34917
gompose <peter.m12321@gmail.com>
2017-01-11 07:08:31 -0500

    Update Reader.java

--- Reader.java
+++ Reader.java
@@ -1,8 +1,29 @@
-import java.swing.*
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class Reader {
-  public static void main (String[] args) {
-  
-  }
+    private String text = "";
+    private int pages = "";
+    private String[][] book = new String[3][pages];
+    public static void fill (String s){
+	// s will be a path
+	// Pull the contents of file s, and set text to it.
+	text = // Insert here
+	    }
+    public static void main (String[] args) {
+	JFrame f = new JFrame();
+	JButton b = new JButton("Next");
+	JButton a = new JButton("Previous");
+	a.setBounds(0, 100, 50, 50);
+	f.add(a);
+	b.setBounds(100, 100, 50, 50);
+	f.add(b);
+	f.setSize(500, 500);
+	f.setLayout(null);
+	f.setVisible(true);
+	
+
+    }
 }
     
