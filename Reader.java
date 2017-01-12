de8329de3b8440edbbb66602be93b86a0ae1ed7b
gompose <peter.m12321@gmail.com>
2017-01-12 05:27:55 -0500

    Implemented Scanner
    In need of debugging

--- Reader.java
+++ Reader.java
@@ -1,17 +1,41 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.*;
+import java.io.*;
+
+
 
 public class Reader {
-    private String text = "";
+    private static String text = "";
     private int pages = 0;
     private String[][] book = new String[3][pages];
+    
+    
     public static void fill (String s){
 	// s will be a path
 	// Pull the contents of file s, and set text to it.
 	//text = // Insert here
 	    }
     public static void main (String[] args) {
+	//File Reading
+
+	try{
+	    Scanner sc = new Scanner(new File("Driver.txt"));
+	    while(sc.hasNext()){
+		text += sc.next();
+	    }
+	}catch (FileNotFoundException e) {
+	    System.out.println("File Not Found!");	    
+	}
+	
+	
+	
+	
+
+
+
+	//GUI
 	JFrame f = new JFrame();
 	JButton b = new JButton("Next");
 	JButton a = new JButton("Previous");
@@ -27,6 +51,9 @@ public class Reader {
 	f.setSize(500, 500);
 	f.setLayout(null);
 	f.setVisible(true);
+	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	
     }
 	
 }
